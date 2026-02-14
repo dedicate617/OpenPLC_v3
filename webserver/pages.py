68ea5dda0d7790f49d96c087c236431dd8e55256
BosMutus <bosmutus@hotmail.com>
2026-02-14 15:52:18 +0800

    Implement DSL runtime flow and binary case study demo

--- webserver/pages.py
+++ webserver/pages.py
@@ -1563,6 +1563,16 @@ add_slave_devices_tail = """
                         <input type='text' style='width: 20%' id='aow_start' name='aow_start' placeholder='0'>
                         <label for='aow_size' style='padding-left:20px'><b>Size:</b></label>
                         <input type='text' style='width: 20%' id='aow_size' name='aow_size' placeholder='8'>
+                        <p style='font-size:14px; margin-top:20px'><b>Discrete Inputs Blocks (one block per line: start,size)</b></p>
+                        <textarea id='di_blocks' name='di_blocks' rows='3' style='width: 90%' placeholder='0,8&#10;16,8'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Coils Blocks (one block per line: start,size)</b></p>
+                        <textarea id='coils_blocks' name='coils_blocks' rows='3' style='width: 90%' placeholder='0,8&#10;16,8'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Input Registers Blocks (one block per line: start,size)</b></p>
+                        <textarea id='ir_blocks' name='ir_blocks' rows='3' style='width: 90%' placeholder='0,4&#10;20,2'></textarea>
+                        <p style='font-size:14px; margin-top:20px'><b>Holding Registers - Read Blocks (one block per line: start,size)</b></p>
+                        <textarea id='hr_read_blocks' name='hr_read_blocks' rows='4' style='width: 90%' placeholder='0,10&#10;100,5'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Holding Registers - Write Blocks (one block per line: start,size)</b></p>
+                        <textarea id='hr_write_blocks' name='hr_write_blocks' rows='4' style='width: 90%' placeholder='0,10&#10;100,5'></textarea>
                     </div>
                     <br>
                     <center><input type="submit" class="button" style="font-weight:bold; width: 310px; height: 53px; margin: 0px 20px 0px 20px;" value='Save device'></center>
@@ -1912,6 +1922,16 @@ edit_slave_devices_tail = """
                         <input type='text' style='width: 20%' id='aow_start' name='aow_start' placeholder='0'>
                         <label for='aow_size' style='padding-left:20px'><b>Size:</b></label>
                         <input type='text' style='width: 20%' id='aow_size' name='aow_size' placeholder='8'>
+                        <p style='font-size:14px; margin-top:20px'><b>Discrete Inputs Blocks (one block per line: start,size)</b></p>
+                        <textarea id='di_blocks' name='di_blocks' rows='3' style='width: 90%' placeholder='0,8&#10;16,8'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Coils Blocks (one block per line: start,size)</b></p>
+                        <textarea id='coils_blocks' name='coils_blocks' rows='3' style='width: 90%' placeholder='0,8&#10;16,8'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Input Registers Blocks (one block per line: start,size)</b></p>
+                        <textarea id='ir_blocks' name='ir_blocks' rows='3' style='width: 90%' placeholder='0,4&#10;20,2'></textarea>
+                        <p style='font-size:14px; margin-top:20px'><b>Holding Registers - Read Blocks (one block per line: start,size)</b></p>
+                        <textarea id='hr_read_blocks' name='hr_read_blocks' rows='4' style='width: 90%' placeholder='0,10&#10;100,5'></textarea>
+                        <p style='font-size:14px; margin-top:10px'><b>Holding Registers - Write Blocks (one block per line: start,size)</b></p>
+                        <textarea id='hr_write_blocks' name='hr_write_blocks' rows='4' style='width: 90%' placeholder='0,10&#10;100,5'></textarea>
                     </div>
                     <br>
                     <center><input type="submit" class="button" style="font-weight:bold; width: 310px; height: 53px; margin: 0px 20px 0px 20px;" value='Save device'><a href='delete-device?dev_id="""
